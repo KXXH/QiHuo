469915b849a2b5ef7e58c4de5677dd98ccd0e660
jboshu <625491845@qq.com>
2019-05-21 23:41:29 +0800

    总和1

---
+++ law_test/src/warehouse/change/changeAction.java
@@ -0,0 +1,77 @@
+package warehouse.change;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import user.manager.User;
+import utils.dbOpener;
+import utils.tokenChecker;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.*;
+import java.io.PrintWriter;
+
+
+@WebServlet(name = "changeAction")
+public class changeAction extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+
+        String stockid = request.getParameter("StockId");
+        //String stockname = request.getParameter("StockName");
+        String quantity = request.getParameter("Quantity");
+        String bunitprice = request.getParameter("BUnitPrice");
+        String tocken = request.getParameter("Cookie");
+        System.out.println(tocken);
+
+        User user = tokenChecker.tokenToUser(tocken);
+
+        request.setCharacterEncoding("UTF-8");
+        System.out.println("页面传递过来的数据获取完毕");
+
+        //开始连接数据库，需要先把mysql-connector-java-5.0.4-bin.jar和json.jar拷贝到ROOT/WEB-INF/lib下
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException classnotfoundexception) {
+            classnotfoundexception.printStackTrace();
+        }
+        System.out.println("加载了JDBC驱动");
+
+        //然后链接数据库，开始操作数据表
+        try {
+            Connection conn = dbOpener.getDB();
+            System.out.println("准备statement。");
+            Statement statement = conn.createStatement();
+            System.out.println("已经链接上数据库！");
+
+            String sql1 = "update tbl_userrealwh set Quantity='" + quantity + "' WHERE UserId='"+user.getUserId()+"' and StockId='"+stockid+"'";
+            System.out.println("sql1:"+sql1);
+            statement.executeUpdate(sql1);
+
+            String sql = "update tbl_userwh set Quantity='" + quantity
+                        + "',BUnitPrice='" + bunitprice+ "' WHERE UserId='"+user.getUserId()+"' and StockId='"+stockid+"'";
+            System.out.println("sql"+sql);
+            System.out.println("即将执行的SQL语句是：" + sql);
+            statement.executeUpdate(sql);
+            statement.close();
+            conn.close();
+
+            System.out.println("操作数据完毕，关闭了数据库！");
+
+        } catch (SQLException sqlexception) {
+            sqlexception.printStackTrace();
+
+        }
+
+        System.out.println("页面执行完毕！");
+    }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
---
+++ law_test/web/js/register.js
@@ -0,0 +1,41 @@
+﻿function add(){
+    window.location="register.html";
+}
+function modify(){
+    window.location="change_list.html";
+}
+function query(){
+    window.location="query_list.html";
+}
+
+
+
+function addstock(){
+
+	var url = "/registerAction";
+    var stockid = document.getElementById("StockId").value;
+    var stockname = document.getElementById("StockName").value;
+    var quantity = document.getElementById("Quantity").value;
+    var bunitprice = document.getElementById("BUnitPrice").value;
+
+    var coo = getCookie("tocken");
+    var data1 = '{"StockId":"'+stockid+'","StockName":"'+stockname+'","Quantity":"'+quantity+'","BUnitPrice":"'+bunitprice+'","Cookie":"'+coo+'"}';
+    var obj = JSON.parse(data1);
+
+    $.post(url,obj,function(json){
+        console.log(JSON.stringify(json));
+        //initMenu(json);
+    })
+    if(stockid=="" || stockname=="" || quantity==""|| bunitprice=="")
+    {
+        alert("输入不正确,请重新输入!");
+    }
+    else
+    {
+        alert("买入成功!");
+    }
+
+
+
+}
+
