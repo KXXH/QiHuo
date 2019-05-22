849479564aede230928efb414d6706cc42d207ed
jboshu <625491845@qq.com>
2019-05-22 22:58:47 +0800

    交易模块查询功能(管理员)
    1.增加查询功能
    2.优化原持仓管理的查询功能

--- law_test/src/business/query/buschaxunAction.java
+++ law_test/src/business/query/buschaxunAction.java
@@ -1,4 +1,4 @@
-package warehouse.query;
+package business.query;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -20,22 +20,24 @@ import java.util.List;
 import java.util.Map;
 
 
-@WebServlet(name = "chaxunAction")
-public class chaxunAction extends HttpServlet {
+@WebServlet(name = "buschaxunAction")
+public class buschaxunAction extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
 
         List jsonList = new ArrayList();
         //获取add_file.jsp页面提交后传递过来的参数，在form里的参数才能传递过来，注意name和id的区别
 
-
+        String orderid = request.getParameter("OrderId");
+        String userid = request.getParameter("UserId");
+        String username = request.getParameter("UserName");
         String stockid = request.getParameter("StockId");
         String stockname = request.getParameter("StockName");
-        String tocken = request.getParameter("Cookie");
-        System.out.println(tocken);
+        //String tocken = request.getParameter("Cookie");
+        //System.out.println(tocken);
 
 
-        User user = tokenChecker.tokenToUser(tocken);
+        //User user = tokenChecker.tokenToUser(tocken);
 
         try {
             Class.forName("com.mysql.jdbc.Driver");
@@ -49,7 +51,7 @@ public class chaxunAction extends HttpServlet {
             //链接数据库，IP地址是localhost，端口是3306，账号和密码是ylx，这些都可以更改
             Connection conn = dbOpener.getDB();
             System.out.println("链接完毕，开始创建准备数据库操作的statement<br>");
-            String sql = "select * from tbl_userrealwh WHERE UserName=? and StockId=? and StockName=?";
+            String sql = "select * from tbl_userwh WHERE UserId=? or UserName=? or StockId=? or StockName=? or OrderId=?";
             System.out.println("即将执行的SQL语句是：" + sql);
 
             /*PreparedStatement statement = conn.createStatement();
@@ -74,23 +76,33 @@ public class chaxunAction extends HttpServlet {
             //加个断行
             System.out.println("<br>");
             System.out.println("====================显示完毕====================<br>");*/
-
+            System.out.println(userid);
+            System.out.println(username);
             System.out.println(stockid);
+            System.out.println(stockname);
+            System.out.println(orderid);
             PreparedStatement ps;
             try {
                 ps = conn.prepareStatement(sql);    //实例化PreparedStatement对象
-                ps.setString(1,user.getUserName());
-                ps.setString(2,stockid);       //设置预处理语句参数
-                ps.setString(3,stockname);
+                ps.setString(1,userid);
+                ps.setString(2,username);
+                ps.setString(3,stockid);       //设置预处理语句参数
+                ps.setString(4,stockname);
+                ps.setString(5,orderid);
                 ResultSet rs = ps.executeQuery();   //执行预处理语句获取查询结果集
                 System.out.println("执行完毕，逐条显示<br>");
                 //如果查询有结果，则循环显示查询出来的记录
                 System.out.println("<br>====================开始输出====================");
-                while(rs.next()){       //循环遍历查询结果集
+                while(rs.next()){
                     JSONObject json = new JSONObject();
+                    json.put("orderid",rs.getString("OrderId"));
+                    json.put("userid",rs.getString("UserId"));
+                    json.put("username",rs.getString("UserName"));
                     json.put("stockid",rs.getString("StockId"));
                     json.put("stockname",rs.getString("StockName"));
                     json.put("quantity",rs.getString("Quantity"));
+                    json.put("bunitprice",rs.getString("BUnitPrice"));
+                    json.put("createat",rs.getString("CreateAt"));
 
                 jsonList.add(json);
                 }
@@ -119,6 +131,7 @@ public class chaxunAction extends HttpServlet {
         } catch (JSONException e) {
             e.printStackTrace();
         }
+
         System.out.println("最后构造得到的json是："+jsonObject.toString());
         response.setContentType("application/json; charset=UTF-8");
         try {
