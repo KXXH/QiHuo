5ad7335f738d9a1de8313db111be95d9225422b8
Zhu Jiamin <32239719+KXXH@users.noreply.github.com>
2019-05-22 22:37:04 +0800

    增加权限管理说明
    请仔细阅读permissionChecker.java文件的注释并修改权限表

--- law_test/src/permission/manager/permissionChecker.java
+++ law_test/src/permission/manager/permissionChecker.java
@@ -17,7 +17,28 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Created by zjm97 on 2019/5/22.
  */
+
+/**
+ * 权限检查功能，在servlet中应用权限管理只需要在开头加上下面一行代码即可。
+ * if(!permissionChecker.checkPermissionAndResponse(request,response,this)) return;
+ * 权限管理器会自动访问数据库检查该servlet对应的权限和用户权限是否匹配，如不匹配还会自动发送错误信息。
+ * 权限码各位含义：
+ * |位数 |    含义       |
+ * | 1  |未登录用户      |
+ * | 2  |unchecked用户  |
+ * | 3  |normal用户     |
+ * | 4  |admin管理员     |
+ * | 5  |super_admin超管 |
+ * 典型的权限码对照表：
+ * 31(11111):对所有用户开放
+ * 24(11000):仅对管理员及以上用户开放
+ * 28(11100):仅对已经验证邮箱的用户及以上权限开放
+ * 4(00100):仅对已经验证邮箱的用户开放
+ * 2(00010):仅对尚未验证邮箱，且已经注册的用户开放
+ * 1(00001):仅对尚未注册的用户开放
+ */
 public class permissionChecker {
+
     public static boolean checkPermission(javax.servlet.http.HttpServlet servlet,User user) throws SQLException {
         String classname=servlet.getClass().getCanonicalName();
         System.out.println("classname="+classname);
